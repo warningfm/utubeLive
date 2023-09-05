a297f1659e82a0fe24ac29888991d200c8edde37
Radio Warning FM <warningfm@gmail.com>
2023-09-05 18:12:51 +0700

    Update TVDI.py

--- scripts/TVDI.py
+++ scripts/TVDI.py
@@ -38,7 +38,7 @@ def grab(url):
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=7680000')
 s = requests.Session()
 with open('../TVDI_info.txt') as f:
     for line in f:
