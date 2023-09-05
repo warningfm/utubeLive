01985247c55b49371c4ff1a229d849d59c18213c
Radio Warning FM <warningfm@gmail.com>
2023-09-05 22:25:15 +0700

    Update and rename SukeTV.py to AlQuranKareem.py

--- scripts/AlQuranKareem.py
+++ scripts/AlQuranKareem.py
@@ -40,7 +40,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1280000,FRAME-RATE=30')
 s = requests.Session()
-with open('../SukeTV_info.txt') as f:
+with open('../AlQuranKareem_info.txt') as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
