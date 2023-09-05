9a31c2e5a844f29aa407febbdea1adc441039599
Radio Warning FM <warningfm@gmail.com>
2023-09-05 17:05:53 +0700

    Update tvonenews.py

--- scripts/tvonenews.py
+++ scripts/tvonenews.py
@@ -38,9 +38,9 @@ def grab(url):
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1596000')
 s = requests.Session()
-with open('../BuletinTV3_info.txt') as f:
+with open('../tvonenews_info.txt') as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
