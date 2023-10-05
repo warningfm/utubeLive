1fd0b94ed4d39ce3ca5890f1949a64b94905e829
Radio Warning FM <warningfm@gmail.com>
2023-10-06 03:14:32 +0700

    Update nagaswarafmbgr.sh

--- nagaswarafmbgr.sh
+++ nagaswarafmbgr.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 MuseMalaysia.py > ../nagaswarafmbgr.m3u8
+python3 nagaswarafmbgr.py > ../nagaswarafmbgr.m3u8
 
 echo m3u grabbed
